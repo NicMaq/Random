a6ae8bcf7cff7e68bef8b2561a692a4b818fe845
NicMaq <nicolas.maquaire@gmail.com>
2022-10-29 20:14:10 -0700

    Halloween Skull

--- BlenderScriptBallsFall.py
+++ BlenderScriptBallsFall.py
@@ -178,13 +178,20 @@ def _initScene():
                 if area.type == 'VIEW_3D':
                     with context.temp_override(window=window, area=area):
                         
-                        bpy.ops.object.select_all(action='SELECT')
-                        bpy.ops.object.delete(use_global=False, confirm=False)                    
+                        bpy.ops.object.select_all(action="SELECT")
+                        obTCart = bpy.context.scene.objects["templateCart"]
+                        obTCart.select_set(False)
+                        obTSkull = bpy.context.scene.objects["templateSkull"]
+                        obTSkull.select_set(False)                    
+                        bpy.ops.object.delete(use_global=False, confirm=False)                     
 
                         # Add ground and back wall
                         bpy.ops.mesh.primitive_plane_add(size=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 0))
                         bpy.context.object.name = "Ground"
                         bpy.ops.transform.resize(value=(100, 100, 100), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=False, use_snap_edit=False, use_snap_nonedit=False, use_snap_selectable=False)
+                        #bpy.ops.rigidbody.object_add()
+                        #bpy.context.object.rigid_body.collision_shape = 'BOX'
+                        #bpy.context.object.rigid_body.type = 'PASSIVE'
 
                         bpy.ops.mesh.primitive_plane_add(size=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 0))
                         bpy.context.object.name = "Sky"
@@ -193,13 +200,19 @@ def _initScene():
                         bpy.ops.transform.translate(value=(10, 0, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=False, use_snap_edit=False, use_snap_nonedit=False, use_snap_selectable=False)
 
                         # Add cart (cube)
-                        bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(0, 0, 1), scale=(3, 3, 1))
-                        bpy.context.object.name = "Cart"
+                        obTemplateCart = bpy.data.objects["templateCart"]
+                        obCart = bpy.data.objects.new('Cart', obTemplateCart.data)
+                        bpy.data.collections["Collection"].objects.link(obCart)
+                        bpy.context.view_layer.objects.active = obCart   # Make the Cart the active object 
+                        bpy.ops.rigidbody.object_add()
+                        bpy.context.object.rigid_body.type = 'PASSIVE'
+                        bpy.context.object.rigid_body.collision_shape = 'MESH'
 
                         # Add camera
                         bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(0, 0, 0), rotation=(1.11569, 0.0131949, -2.41231), scale=(1, 1, 1))
                         bpy.context.object.name = "Camera"
                         bpy.context.area.spaces.active.region_3d.view_perspective = 'CAMERA'
+                        #bpy.ops.view3d.object_as_camera()
                         bpy.context.object.rotation_euler[2] = -1.5708
                         bpy.context.object.rotation_euler[0] = 1.5708
                         bpy.ops.transform.translate(value=(-50, -0, 6), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=False, use_snap_edit=False, use_snap_nonedit=False, use_snap_selectable=False)
@@ -208,9 +221,15 @@ def _initScene():
                         bpy.ops.object.light_add(type='SUN', radius=1, align='WORLD', location=(10, 0, 50), scale=(1, 1, 1))
                         
                         # Add new ball
-                        y = np.random.randint(20, size=1)[0]
-                        bpy.ops.mesh.primitive_uv_sphere_add(enter_editmode=False, align='WORLD', location=(0, y, 16), scale=(1, 1, 1))
-                        bpy.context.object.name = "Ball"
+                        obSkull = bpy.data.objects["templateSkull"]
+                        obBall = bpy.data.objects.new('Ball', obSkull.data)
+                        bpy.data.collections["Collection"].objects.link(obBall)
+                        
+                        y = np.random.randint(40, size=1)[0] - 20
+                        obBall.location.x = 0
+                        obBall.location.y = y
+                        obBall.location.z = 16
+                        bpy.context.view_layer.objects.active = obBall   # Make the Cart the active object 
                         bpy.ops.rigidbody.object_add()
                         bpy.context.object.rigid_body.collision_shape = 'SPHERE'                      
                         
@@ -239,27 +258,30 @@ def _initScene():
                         bpy.data.materials['MatSky'].diffuse_color = (0.0029881, 0.477195, 0.8, 1) #change color  
 
                         matBall = bpy.data.materials.new(name="MatBall") #set new material to variable
-                        bpy.data.materials['MatBall'].diffuse_color = (0.8, 0, 0.000847888, 1) #change color  
+                        bpy.data.materials['MatBall'].diffuse_color = (0.698773, 0.8, 0.759914, 1) #change color  
 
                         # rename data block and link material to object
                         obCart = bpy.context.scene.objects["Cart"]       # Get the object
                         obCart.data.name = "Cart"
+                        bpy.context.view_layer.objects.active = obCart   # Make the Cart the active object 
                         obCart.data.materials.append(matCart) #add the material to the object
                                 
                         obGround = bpy.context.scene.objects["Ground"]       # Get the object
                         obGround.data.name = "Ground"
+                        bpy.context.view_layer.objects.active = obGround   # Make the Cart the active object 
                         obGround.data.materials.append(matGround) #add the material to the object
 
                         obSky = bpy.context.scene.objects["Sky"]       # Get the object
                         obSky.data.name = "Sky"
+                        bpy.context.view_layer.objects.active = obSky   # Make the Cart the active object 
                         obSky.data.materials.append(matSky) #add the material to the object
                         
                         obBall = bpy.context.scene.objects["Ball"]       # Get the object
                         obBall.data.name = "Ball"      
-                        obBall.data.materials.append(matBall) #add the material to the object             
-
-                        bpy.context.view_layer.objects.active = None
-                        bpy.ops.object.select_all(action='DESELECT')       
+                        obBall.data.materials.append(matBall) #add the material to the object   
+                        
+                        bpy.ops.object.select_all(action='DESELECT')  
+                        bpy.context.view_layer.objects.active = None                         
                         
                         break
 
@@ -275,7 +297,7 @@ def _reset(config):
     objectCart = bpy.data.objects["Cart"]
 
     # Reset Ball location
-    y = np.random.randint(20, size=1)[0]
+    y = np.random.randint(30, size=1)[0] - 15
     objectBalls.location = (0, y, 16)
 
     # Reset Cart location
@@ -367,7 +389,7 @@ def _launchBalls():
     objectBalls = bpy.data.objects["Ball"]
 
     # Reset Ball location
-    y = np.random.randint(20, size=1)[0]
+    y = np.random.randint(30, size=1)[0] - 15
     objectBalls.location = (0, y, 16)
 
     _printActiveAndSelectedObjects('end launch')
@@ -395,6 +417,10 @@ def _isGroundCollision(loc):
     log.info(f'{"_isGroundCollision is: {} ".format(collided)}')
 
     return collided
+
+def thread_gif(path, frames):
+
+    _generate_gif(path,frames)
 
 
 def _generate_gif(path_name, frames):
@@ -429,6 +455,8 @@ if __name__ == '__main__':
     episode_reward = 0
     max_reward = -10
     frames = []
+    frames_gif = []
+    path_name = ""
 
     log.info("Thread simulate starting")
     #print('Thread simulate starting')
@@ -446,6 +474,7 @@ if __name__ == '__main__':
     scn.frame_set(currentFrame)
 
     simulationFile = "{}/simuBlender.png".format(SAVE_DIR)
+    transparentFile = "{}/transparent.png".format(SAVE_DIR)
     scn.render.filepath = simulationFile    
 
     _initScene()
@@ -479,7 +508,7 @@ if __name__ == '__main__':
                     # Save the render
                     bpy.ops.render.render( write_still=True )
                     image = cv2.imread(simulationFile)
-                    frames.append(image)
+                    frames.append(image.copy())
 
                     q_2blender.task_done()
 
@@ -490,9 +519,6 @@ if __name__ == '__main__':
                     log.info("q_2bonsai - End Put Reset_Bonsai")
                     q_2bonsai.join()    
                     log.info("End Reset_Blender")  
-
-                    #currentFrame += 1
-                    #scn.frame_set(currentFrame)
                 
                 if msgFromBonsai[0] == 'Step_Blender':
 
@@ -510,7 +536,16 @@ if __name__ == '__main__':
                     # Save the render
                     bpy.ops.render.render( write_still=True )
                     image = cv2.imread(simulationFile)
-                    frames.append(image)
+                    image = np.flip(image, axis=2)
+
+                    #Add text
+                    b,g,r,a = 255,0,0,0
+                    text = "Team Bonsai: %s" % (episode_reward) 
+                    image = np.ascontiguousarray(image, dtype=np.uint8)
+                    cv2.putText(image,  text, (20,530), cv2.FONT_HERSHEY_SIMPLEX, 1, (b,g,r), 1, cv2.LINE_AA)
+
+                    frames.append(image.copy())
+
 
                     if info['collided']:
                         currentFrame = scn.frame_start
@@ -526,7 +561,9 @@ if __name__ == '__main__':
                             if max_reward <= episode_reward:
                                 max_reward = episode_reward
                                 path_name = SAVE_DIR + '/' + now + '_Blender_Balls_' + str(episode_reward) + '.gif'
-                                _generate_gif(path_name, frames) 
+                                frames_gif = frames.copy()
+                                threadGif = threading.Thread(target=thread_gif, args=(path_name,frames_gif,))
+                                threadGif.start() 
 
                     if currentFrame > scn.frame_end: 
                         truncated = True 
